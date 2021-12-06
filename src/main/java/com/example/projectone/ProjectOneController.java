ffd6bf504dd6a1f825ce1cac3e7dd1adb0e626b4
Kermo <raja.kermo@gmail.com>
2021-12-06 14:21:21 +0200

    Lisasin need

---
+++ src/main/java/com/example/projectone/ProjectOneController.java
@@ -0,0 +1,12 @@
+package com.example.projectone;
+
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+
+public class ProjectOneController {
+
+
+
+
+
+}
---
+++ src/main/java/com/example/projectone/ProjectOneRepository.java
@@ -0,0 +1,9 @@
+package com.example.projectone;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProjectOneRepository {
+
+
+}
