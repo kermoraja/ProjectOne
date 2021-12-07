f5b169c0dca6c65a9301f07275949606449e28dd
Kermo <raja.kermo@gmail.com>
2021-12-07 16:36:32 +0200

    tour_main database test

--- src/main/java/com/example/projectone/ProjectOneService.java
+++ src/main/java/com/example/projectone/ProjectOneService.java
@@ -1,7 +1,16 @@
 package com.example.projectone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProjectOneService {
+@Autowired
+private ProjectOneRepository projectOneRepository;
+    @PostMapping("")
+    public void createTour(@RequestBody Tour tour) {
+       projectOneRepository.createTour(tour);
+    }
 }
--- src/main/java/com/example/projectone/Tour.java
+++ src/main/java/com/example/projectone/Tour.java
@@ -2,7 +2,49 @@ package com.example.projectone;
 
 public class Tour {
 
-private String desc_short;
+    private String desc_short;
+    private String desc_long;
+    private String duration;
+    private int city;
+    private String title;
 
+    public String getDesc_short() {
+        return desc_short;
+    }
 
+    public void setDesc_short(String desc_short) {
+        this.desc_short = desc_short;
+    }
+
+    public String getDesc_long() {
+        return desc_long;
+    }
+
+    public void setDesc_long(String desc_long) {
+        this.desc_long = desc_long;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public int getCity() {
+        return city;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
