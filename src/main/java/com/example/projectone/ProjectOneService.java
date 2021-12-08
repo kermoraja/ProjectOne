d08ba1ef3599bcbd7575bd702e578daaaa9d74ff
Kermo <raja.kermo@gmail.com>
2021-12-08 23:42:50 +0200

    lisasin veits asju

--- src/main/java/com/example/projectone/ProjectOneService.java
+++ src/main/java/com/example/projectone/ProjectOneService.java
@@ -2,15 +2,27 @@ package com.example.projectone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProjectOneService {
-@Autowired
-private ProjectOneRepository projectOneRepository;
-    @PostMapping("")
-    public void createTour(@RequestBody Tour tour) {
-       projectOneRepository.createTour(tour);
+    @Autowired
+    private ProjectOneRepository projectOneRepository;
+
+    @PostMapping("api/tour")
+    public int createTour(@RequestBody Tour tour) {
+        return projectOneRepository.createTour(tour);
+    }
+
+    @PostMapping("api/addphoto")
+    public void addPhoto(@RequestBody TourPhotos tourPhotos) {
+         projectOneRepository.addPhoto(tourPhotos);
+    }
+    @GetMapping("api/tour/{id}")
+    public TourDto getTour(@PathVariable("id") Integer id) {
+        return projectOneRepository.getTour(id);
     }
 }
---
+++ src/main/java/com/example/projectone/TourPhotos.java
@@ -0,0 +1,32 @@
+package com.example.projectone;
+
+public class TourPhotos {
+
+    private String photo_title;
+    private String photo_url;
+    private int tour_id;
+
+    public String getPhoto_title() {
+        return photo_title;
+    }
+
+    public void setPhoto_title(String photo_title) {
+        this.photo_title = photo_title;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    public int getTour_id() {
+        return tour_id;
+    }
+
+    public void setTour_id(int tour_id) {
+        this.tour_id = tour_id;
+    }
+}
